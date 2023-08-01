59932a56201522129fcc64f2916eff2d6e6396b8
green <grzx2210@163.com>
2023-08-01 14:29:21 +0800

    Update train.py

--- chat/train.py
+++ chat/train.py
@@ -109,7 +109,7 @@ def main():
         f"Training on the following datasets and their proportions: {[split + ' : ' + str(dset.num_rows) for split, dset in raw_datasets.items()]}"
     )
     with training_args.main_process_first(desc="Log a few random samples from the raw training set"):
-        for index in random.sample(range(len(raw_datasets["train_fit"])), 3):
+        for index in random.sample(range(len(raw_datasets["train"])), 3):
             logger.info(f"Sample {index} of the raw training set:\n\n{raw_datasets['train'][index]['messages']}")
 
     #########################
@@ -133,13 +133,13 @@ def main():
     logger.info(f"Added {num_added_tokens} new tokens: {dialogue_tokens}")
 
     if training_args.do_train:
-        column_names = list(raw_datasets["train_fit"].features)
+        column_names = list(raw_datasets["train"].features)
     else:
         column_names = list(raw_datasets["test"].features)
     text_column_name = "text" if "text" in column_names else column_names[0]
 
     with training_args.main_process_first(desc="Log a few random samples from the training set"):
-        for index in random.sample(range(len(raw_datasets["train_fit"])), 3):
+        for index in random.sample(range(len(raw_datasets["train"])), 3):
             logger.info(f"Sample {index} of the raw training set:\n\n{raw_datasets['train'][index]['text']}")
 
     # since this will be pickled to avoid _LazyModule error in Hasher force logger loading before tokenize_function
@@ -218,9 +218,9 @@ def main():
         )
 
     if training_args.do_train:
-        if "train_fit" not in tokenized_datasets:
+        if "train" not in tokenized_datasets:
             raise ValueError("--do_train requires a train dataset")
-        train_dataset = lm_datasets["train_fit"]
+        train_dataset = lm_datasets["train"]
         if data_args.max_train_samples is not None:
             max_train_samples = min(len(train_dataset), data_args.max_train_samples)
             train_dataset = train_dataset.select(range(max_train_samples))
@@ -281,8 +281,8 @@ def main():
         )
         metrics["train_samples"] = min(max_train_samples, len(train_dataset))
 
-        trainer.log_metrics("train_fit", metrics)
-        trainer.save_metrics("train_fit", metrics)
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
         trainer.save_state()
 
     ##########
